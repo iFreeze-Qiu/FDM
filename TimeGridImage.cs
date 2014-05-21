86ee63dc2c0254e0547f6c87113b39c62ca979ad
fkbyf <a462611226@gmail.com>
2014-05-21 16:33:19 +0400

    TimeGridImage implemented IGridImage

--- TimeGridImage.cs
+++ TimeGridImage.cs
@@ -6,39 +6,51 @@ using System.IO;
 
 namespace FiniteDifferenceMethod
 {
-    class TimeGridImage
+    class TimeGridImage : IGridImage
     {
         private static readonly string DEFAULT_DIRECTORY_NAME = "Data";
         private static double STEP = 0.001;
 
-        private static List<GridImage> gridImageList = new List<GridImage>();
-        private static List<double> time = new List<double>();
+        private List<GridImage> gridImageList = new List<GridImage>();
+        private List<double> time = new List<double>();
 
-        public static void loadGridImages()
+        private GridImage currentGridImage;
+
+        private TimeGridImage(List<GridImage> gridImageList, List<double> time)
+        {
+            this.gridImageList = gridImageList;
+            this.time = time;
+            currentGridImage = gridImageList[1];
+        }
+
+        public static TimeGridImage loadGridImages()
         {
             int index = 0;
             string directoryName = DEFAULT_DIRECTORY_NAME + Path.DirectorySeparatorChar + index.ToString();
+            List<GridImage> gridImageList = new List<GridImage>();
+            List<double> time = new List<double>();
 
             if (!Directory.Exists(directoryName))
             {
                 //TODO: exception
                 Console.WriteLine("No time data");
-                return;
+                return null;
             }
 
             while (Directory.Exists(directoryName))
             {
                 Console.WriteLine("Loading data from " + directoryName);
                 gridImageList.Add(GridImage.LoadFromProject(directoryName));
-                
+
                 time.Add(STEP * index);
-                
+
                 index++;
                 directoryName = DEFAULT_DIRECTORY_NAME + Path.DirectorySeparatorChar + index.ToString();
             }
+            return new TimeGridImage(gridImageList, time);
         }
 
-        public static GridImage getGridImage(int index)
+        public GridImage getGridImage(int index)
         {
             if (index >= gridImageList.Count)
             {
@@ -50,5 +62,95 @@ namespace FiniteDifferenceMethod
             return gridImageList[index];
         }
 
+        //------------------------------------ ЗАПОЛНИТЬ!---------------------
+        public bool IsATruncatedByColor
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsBTruncatedByColor
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsMTruncatedByColor
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsJTruncatedByColor
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IConverter Converter
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public int Width
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public int Height
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public int Depth
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public float Step
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IGrid ConvertToGrid()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int[] GetLayerX(VariableType variable, int x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetLayerX(int[] data, VariableType variable, int x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int[] GetLayerY(VariableType variable, int y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetLayerY(int[] data, VariableType variable, int y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int[] GetLayerZ(VariableType variable, int z)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetLayerZ(int[] data, VariableType variable, int z)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetPoint(VariableType variable, int x, int y, int z)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(string projectName)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
